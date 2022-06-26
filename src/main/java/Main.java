7884066cb5b3992957e74aca4424337c48016958
Kengi <manfri.crapa@gmail.com>
2022-06-26 13:08:24 +0200

    Hello

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,16 @@ public class Main {
         Person person1 = new Person("Amerigo");
 
         System.out.println(person.getName() + " " + person1.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
+        System.out.println(person.getName());
 
 
     }
