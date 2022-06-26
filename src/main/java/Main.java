bf2a5df3092fbaaa2ab94ea770146cd51b096f6e
Kengi <manfri.crapa@gmail.com>
2022-06-26 12:58:38 +0200

    Hello

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Amigos");
+
+        Person person = new Person("Jamila");
+        Person person1 = new Person("Amerigo");
+
+        System.out.println(person.getName() + " " + person1.getName());
+
+
+    }
+}
